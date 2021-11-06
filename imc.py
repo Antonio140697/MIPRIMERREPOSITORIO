aee3a83c60fba37dde9a3bad33e40aa1ed3c25ed
jose antonio <anthony140697@gmail.com>
2021-11-06 12:20:27 -0600

    ingreso de la segunda condicion con el valor menor o igual a 19

--- imc.py
+++ imc.py
@@ -3,5 +3,7 @@ altura=float(input("ingresa tu altura en metros"))
 imc=masa/altura**2
 print("tu imc es: "+str(imc))
 
-if imc>25 :
+if imc>=25 :
     print("sobrepeso")
+if imc<=19 :
+    print("desnutricion")
