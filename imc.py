c7a27e14c7da7ff6356c0ff678c39db6dbf7a76a
jose antonio <anthony140697@gmail.com>
2021-11-06 12:17:09 -0600

    ingreso de la primera condicion con valor mayor a 25

--- imc.py
+++ imc.py
@@ -2,3 +2,6 @@ masa=float(input("ingresa tu masa kilogramos"))
 altura=float(input("ingresa tu altura en metros"))
 imc=masa/altura**2
 print("tu imc es: "+str(imc))
+
+if imc>25 :
+    print("sobrepeso")
